b5c3c73053a5dc350b86c3aa49641c2237294d41
VMois <vmois@protonmail.com>
2023-11-10 17:41:12 +0100

    Adjust my solution after watching The Primeagen

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,66 +1,56 @@
+const dir = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+];
 
-function walk(maze: string[], wall: string, end: Point, seen: boolean[][], path: Point[]): boolean {
-    
-    let point = path[path.length - 1];
-
+function walk(maze: string[], wall: string, end: Point, point: Point, seen: boolean[][], path: Point[]): boolean {
     // 1. Did we hit a wall?
     if (maze[point.y][point.x] === wall) {
-        path.pop()
         return false;
     }
 
     // 2. Are we off the grid?
     if (point.x >= maze[0].length || point.y >= maze.length) {
-        path.pop()
         return false;
     }
 
     // 3. Did we find an exit?
     if (point.x === end.x && point.y === end.y) {
+        path.push({x: point.x, y: point.y});
         return true;
     }
 
     // 4. Have we seen this cell before?
     if (seen[point.y][point.x]) {
-        path.pop()
         return false;
     }
 
+    // pre recursion
     seen[point.y][point.x] = true;
+    path.push({x: point.x, y: point.y});
 
-    // Go up
-    path.push({x: point.x, y: point.y + 1})
-    if (walk(maze, wall, end, seen, path)) {
-        return true
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        const nextPoint = {x: point.x + x, y: point.y + y};
+        if (walk(maze, wall, end, nextPoint, seen, path)) {
+            return true;
+        }
     }
 
-    // Go right
-    path.push({x: point.x + 1, y: point.y})
-    if (walk(maze, wall, end, seen, path)) {
-        return true
-    }
-
-    // Go down
-    path.push({x: point.x, y: point.y - 1})
-    if (walk(maze, wall, end, seen, path)) {
-        return true
-    }
-
-    // Go left
-    path.push({x: point.x - 1, y: point.y})
-    if (walk(maze, wall, end, seen, path)) {
-        return true
-    }
+    // post recursion
+    path.pop();
 
     return false;
 } 
 
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
-    let path: Point[] = [start];
+    let path: Point[] = [];
     let seen: boolean[][] = Array(maze.length);
     for (let i = 0; i < seen.length; i++) {
         seen[i] = Array(maze[0].length).fill(false);
     }
-    walk(maze, wall, end, seen, path);
+    walk(maze, wall, end, start, seen, path);
     return path;
 }
