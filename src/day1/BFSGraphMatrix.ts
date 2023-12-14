239399d471103dc0082b23c0e01f8387946443b4
VMois <vmois@protonmail.com>
2023-12-14 19:02:47 +0100

    Add small adjustments to BFS on Graph Matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,7 +1,9 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
-    let seen: boolean[] = new Array(graph.length).fill(true, 0, 1).fill(false, 1);
+    let seen: boolean[] = new Array(graph.length).fill(false);
     let prev: number[] = new Array(graph.length).fill(-1);
     let Q: number[] = [source];
+
+    seen[source] = true;
 
     while (Q.length) {
         let curr = Q.shift() as number;
@@ -11,8 +13,8 @@ export default function bfs(graph: WeightedAdjacencyMatrix, source: number, need
         }
         
         for (let i = 0; i < graph[0].length; i++) {
-            let w = graph[curr][i];
-            if (!seen[i] && w > 0) {
+            let weight = graph[curr][i];
+            if (!seen[i] && weight > 0) {
                 Q.push(i);
                 prev[i] = curr;
             }
@@ -21,11 +23,11 @@ export default function bfs(graph: WeightedAdjacencyMatrix, source: number, need
 
     let path: number[] = [];
     let i = needle;
-    while (seen[i] && prev[i] != -1) {
+    while (prev[i] != -1) {
         path.unshift(i);
         i = prev[i];
     }
-    
+
     if (path.length) {
         path.unshift(source);
         return path;
