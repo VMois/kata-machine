1a283877af631540aee0dc6d5e21ac8db46e1977
VMois <vmois@protonmail.com>
2023-12-14 17:49:26 +0100

    Generate DikjstraList problem

--- ligma.config.js
+++ ligma.config.js
@@ -6,6 +6,7 @@ module.exports = {
         "BinarySearchList",
         "TwoCrystalBalls",
         "BubbleSort",
+        "DijkstraList",
         "SinglyLinkedList",
         "DoublyLinkedList",
         "Queue",
